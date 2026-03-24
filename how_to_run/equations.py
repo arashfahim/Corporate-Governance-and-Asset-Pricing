6d0d6448d7861d16633ef4b27c88d3381eb92767
arashfahim <47042726+arashfahim@users.noreply.github.com>
2026-03-24 16:14:35 -0400

    fixed mistake in the optimal sigma

--- how_to_run/equations.py
+++ how_to_run/equations.py
@@ -3,7 +3,7 @@ import math
 from scipy import integrate
 from scipy.integrate import solve_bvp
 import datetime as dt
-import os
+
 
 def findMin(X):
     x=np.amin(X)
@@ -35,7 +35,6 @@ class Equation(object):
         self.it = config.Numerical_settings.num_iterations
         self.eps = config.Numerical_settings.Stop_criterion_for_F_ODE
         self.df0 = config.Numerical_settings.df0
-        self.path = os.path.dirname(__file__) #path to save the output file
         # self.y = np.empty(self.N)
         # self.dy = np.empty(self.N)
         # self.ddy = np.empty(self.N)
@@ -51,7 +50,7 @@ class Equation(object):
         """Nonlinearity H: ddy = H(dy,y,x)"""
         raise NotImplementedError
 
-    def optimal(self,t):
+    def optimal(self,X,param):
         """Nonlinearity H: ddy = H(dy,y,x)"""
         raise NotImplementedError
 
@@ -125,6 +124,7 @@ class eqn_Ff(Equation):
 
 
     def optimal(self,t):#def optimal_sigma_rho(self,t):
+        lmbd_ = self.param[3]
         sgm_ = self.param[4]
         rho_ = self.param[5]
         y = np.ones(len(t))
@@ -135,18 +135,18 @@ class eqn_Ff(Equation):
         else:
             temp_swtch = [0]
             temp_swtch.extend(self.swtch)
-            temp_swtch.append(self.swtch[-1]+np.min(self.ddf)-1) # the switch cannot happen at values of Gamma than the minimum of ddf
+            temp_swtch.append(self.swtch[-1]+np.min(self.ddf)-1)
             for indexm, i in enumerate(t):
                 #solver of BVP: scipy.integrate.solve_bvp requires
                 #this to work for t of any size. This is the reason
                 #behind the loop.
-                i__ = self.findD(np.abs(i-self.m))#closest point on the m-grid to t
+                i__ = self.findD(np.abs(i-self.m))#closest point on the grid to t
                 y[indexm] = self.ddf[i__[0]]
                 z[indexm] = self.ddf[i__[0]]
             for index, g in enumerate(temp_swtch[0:-1]):
                 y = np.where((y <= g) & (y > temp_swtch[index+1]), sgm_[index], y)
                 z = np.where((z <= g) & (z > temp_swtch[index+1]), rho_[index], z)
-        return y,z
+        return lmbd_*y,z
     
 
     def bvp_solve(self):
@@ -181,7 +181,7 @@ class eqn_Ff(Equation):
         self.ddy = ddF
         self.__ddw__ = findMin(np.absolute(self.ddy))
         self.__w__ = findMin(np.absolute(self.y-self.line))
-        self.__dw__ = findMin(np.absolute(self.dy+1))
+        self.__dw__ = findMin(self.dy)
 
 
 
@@ -206,7 +206,7 @@ np.absolute(self.y[self.__w__[0]]-self.line[self.__w__[0]])
 
         __x_s__ = findMin(np.absolute(self.dy))
         self.x_s = self.x[__x_s__[0]]#switching points
-        ind = self.N #np.min([int((self.__w__[0]+1)*1.2),self.N])
+        ind = np.min([int((self.__w__[0]+1)*1.2),self.N])
         self.x = self.x[0:ind]
         # print(self.__w__[0]+1)
         self.x_p = self.x[self.__w__[0]+1]#payment point
@@ -219,23 +219,13 @@ np.absolute(self.y[self.__w__[0]]-self.line[self.__w__[0]])
         self.line = self.line[0:ind]
 
 
+
         self.m_s = self.x_s/self.param[3]#switching points in cash reserve variable m
         self.m_p = self.x_p/self.param[3]#payment point in cash reserve variable m
         self.f = self.y
         self.df = self.dy*self.param[3]
         self.ddf = self.ddy*self.param[3]*self.param[3]
         self.fline = [(self.param[0] - self.param[1] * self.param[3] * x)/self.param[2] for x in self.m]
-        data = np.column_stack((self.x, self.y, self.dy, self.ddy, self.line, self.m, self.f, self.df, self.ddf, self.fline))
-        header_text = 'x, F, dF, ddF, Fline, m, f, df, ddf, fline'
-        np.savetxt(self.path+'output.dat',  # Filename
-                data,           # Array data
-                fmt='%.4f',     # Format of data values (4 decimal places)
-                delimiter=',',  # String or character separating columns
-                header=header_text, # Text written at the beginning of the file
-                comments='# '   # String prefixed to header to mark as a comment
-        )        
-
-
         self.sol= {'domain':self.x, 'solution':self.y,'1st_deriv':self.dy,'2nd_deriv':self.dy,'3rd_deriv':self.ddy,'pay_point':self.x_p, 'negot_point':self.x_s}
         opt_sig, opt_rho = self.optimal(self.m)
         self.swtch_pos = np.where(np.abs(np.diff(opt_sig)) > 0)[0]
@@ -287,7 +277,7 @@ class eqn_FfS(eqn_Ff):
         print('BVP for S is solved in',b.total_seconds(),'seconds.\n')
         self.S = self.solution_S.sol(self.mS)[0]
         self.dS = self.solution_S.sol(self.mS)[1]
-        opt_sig, _ = self.optimal(self.mS)
+        opt_sig, opt_rho = self.optimal(self.mS)
         dis_cnt_coeff = 2/(opt_sig*opt_sig)
         self.ddS = (self.param[2] * self.S - self.param[1] * self.mS * self.dS)\
 *dis_cnt_coeff
@@ -300,7 +290,7 @@ class eqn_FfS(eqn_Ff):
 
     def bvp_T(self):
         def eqn_form(t, X):# defining equation for T
-            opt_sig, _ = self.optimal(t)
+            opt_sig, opt_rho = self.optimal(t)
             dis_cnt_coeff = 2/(opt_sig*opt_sig)
             H_ = np.vstack((X[1], dis_cnt_coeff*(-1 + self.param[2]*X[0] -self.param[1]*t * X[1])))
             return H_
@@ -315,7 +305,7 @@ class eqn_FfS(eqn_Ff):
         print('BVP for T is solved in',b.total_seconds(),'seconds.\n')
         self.T = self.solution_T.sol(self.mS)[0]
         self.dT = self.solution_T.sol(self.mS)[1]
-        opt_sig , _ = self.optimal(self.mS)
+        opt_sig , opt_rho = self.optimal(self.mS)
         dis_cnt_coeff = 2/(opt_sig*opt_sig)
         self.ddT = (-1 + self.param[2] * self.T - self.param[1] * self.mS * self.dT)\
 *dis_cnt_coeff
@@ -345,7 +335,7 @@ class eqn_FfS(eqn_Ff):
         dis_cnt_coeff = 2/(opt_sig*opt_sig)
         self.ddC = (- opt_rho + self.param[2] * self.C - self.param[1] * self.mS * self.dC)\
 *dis_cnt_coeff
-        for i in self.swtch_pos:#setting value at place of jumps avoids plotting jumps as a continuous function
+        for i in self.swtch_pos:#setting value at place of jumps avoids ploting jumps as a continuous function
             j = self.findD(np.abs(self.mS - self.mS[i]))
             self.ddC[j[0]] = np.nan
             self.ddC[j[0]+1] = np.nan
@@ -360,15 +350,7 @@ class eqn_FfS(eqn_Ff):
         self.C1=self.param[0]*self.T-self.param[3]*self.S-self.f[0:(self.__w__[0]+1)]#monitoring cost obtained from the equality f(m)=\mu/r T(m) - \lambda*S(m)-monit_cost(m)
         self.dC1=self.param[0]*self.dT-self.param[3]*self.dS-self.df[0:(self.__w__[0]+1)]
         self.ddC1=self.param[0]*self.ddT-self.param[3]*self.ddS-self.ddf[0:(self.__w__[0]+1)]
-        data = np.column_stack((self.S, self.T, self.C))
-        header_text = 'S, T, C'
-        np.savetxt(self.path+'/output_STC.dat',  # Filename
-                data,           # Array data
-                fmt='%.4f',     # Format of data values (4 decimal places)
-                delimiter=',',  # String or character separating columns
-                header=header_text, # Text written at the beginning of the file
-                comments='# '   # String prefixed to header to mark as a comment
-        )        
+
 
 
 
